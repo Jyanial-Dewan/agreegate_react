c5587569655984ad52554c5346212de0d205ba65
kyaching <kyochingmarma@gmail.com>
2025-09-18 17:06:36 +0600

    added react hook form

--- src/pages/LoginAndSignUp/Login.tsx
+++ src/pages/LoginAndSignUp/Login.tsx
@@ -7,73 +7,99 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useAuthContext } from "@/context/AuthContext/useContext";
+import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().min(2).max(50),
+  password: z.string().min(8),
+});
 
 const Login = () => {
   const { setIsLoggedIn } = useAuthContext();
   const navigate = useNavigate();
+  /** Define form */
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  /** Login */
-  const handleLogin = () => {
+  /** Login function */
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
     setIsLoggedIn(true);
-    console.log("login");
     navigate("/");
   };
+
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <Card className="w-full max-w-sm mx-auto">
-        <CardHeader>
-          <CardTitle className="text-2xl text-center">Login</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
-                <Input id="password" type="password" required />
-                <a
-                  href="#"
-                  className="ml-auto text-sm text-gray-500 hover:text-gray-700 underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-
-        <CardFooter className="flex-col gap-2">
-          <Button
-            onClick={handleLogin}
-            type="submit"
-            className="w-full cursor-pointer"
-          >
-            Login
-          </Button>
-          <p>
-            Don't have an account?
-            <Link
-              to={"/signup"}
-              className="text-blue-600 hover:text-blue-800 hover:cursor-pointer underline-offset-4 hover:underline"
-            >
-              Sign Up
-            </Link>
-          </p>
-        </CardFooter>
-      </Card>
+      <Form {...form}>
+        <Card className="w-full max-w-sm mx-auto">
+          <CardHeader>
+            <CardTitle className="text-2xl text-center">Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="m@example.com" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="*********"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full cursor-pointer">
+                Login
+              </Button>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <p>
+              Don't have an account?{" "}
+              <Link
+                to={"/signup"}
+                className="text-blue-600 hover:text-blue-800 hover:cursor-pointer underline-offset-4 hover:underline"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </Form>
     </div>
   );
 };

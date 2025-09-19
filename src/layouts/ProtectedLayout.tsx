2590297cf7f3d0f505dfb86b257a7581de28e4b5
kyaching <kyochingmarma@gmail.com>
2025-09-19 18:43:29 +0600

    added header and dropdown

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header/Header";
 import { Outlet } from "react-router";
 
 export const ProtectedLayout = () => {
   return (
     <div>
+      <Header />
       <Outlet />
       ProtectedLayout
     </div>

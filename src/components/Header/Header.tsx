0d32fdcccaa0476aa51e76a5c669f6300a7c223e
kyaching <kyochingmarma@gmail.com>
2025-09-21 17:44:11 +0600

    change header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,41 +5,39 @@ import Dropdown from "../Dropdown/Dropdown";
 const Header = () => {
   return (
     <div className="flex justify-between items-center h-[3rem] w-full px-[12px] bg-white shadow-md fixed z-40 ">
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-5 items-center">
         {/* <Link to="/">
           <img src={logo} alt="logo" className="h-[2rem] w-auto" />
         </Link> */}
-        <p className="font-semibold">Aggregate React</p>
+        <p className="font-semibold">Aggregate</p>
+        <div className="flex items-center">
+          <NavLink
+            to="/home"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-winter-100 px-2 py-2 rounded-md flex gap-2 items-center hover:bg-hover"
+                : "px-2 py-3  flex gap-2 items-center hover:border-b-2 hover:border-blue-400"
+            }
+          >
+            <p className="font-semibold hidden lg:block transition duration-300 ease-in-out">
+              Home
+            </p>
+          </NavLink>
+          <NavLink
+            to="/About"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-winter-100 px-2 py-2 rounded-md flex gap-2 items-center hover:bg-hover"
+                : "px-2 py-3 flex gap-2 items-center hover:border-b-2 hover:border-blue-400"
+            }
+          >
+            <p className="font-semibold hidden lg:block transition duration-300 ease-in-out">
+              About
+            </p>
+          </NavLink>
+        </div>
       </div>
 
-      <div className="flex gap-1 items-center">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-winter-100 px-2 py-2 rounded-md flex gap-2 items-center hover:bg-hover"
-              : "px-2 py-2 rounded-md flex gap-2 items-center hover:bg-hover"
-          }
-        >
-          <p className="font-semibold hidden lg:block transition duration-300 ease-in-out">
-            Home
-          </p>
-        </NavLink>
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-winter-100 px-2 py-2 rounded-md flex gap-2 items-center hover:bg-hover"
-              : "px-2 py-2 rounded-md flex gap-2 items-center hover:bg-hover"
-          }
-        >
-          <p className="font-semibold hidden lg:block transition duration-300 ease-in-out">
-            About
-          </p>
-        </NavLink>
-
-        {/* <Dropdown /> */}
-      </div>
       <Dropdown />
     </div>
   );

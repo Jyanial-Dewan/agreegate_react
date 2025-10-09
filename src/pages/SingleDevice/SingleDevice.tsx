8dc1d3ef3d2bc54d8d58773e9a1d7a43c4c34b90
kyaching <kyochingmarma@gmail.com>
2025-10-09 18:37:13 +0600

    develop client info

--- src/pages/SingleDevice/SingleDevice.tsx
+++ src/pages/SingleDevice/SingleDevice.tsx
@@ -7,10 +7,11 @@ import type {
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import LocationInfoTable from "./LocationInfoTable";
+// import LocationInfoTable from "./LocationInfoTable";
 import Loader from "@/components/common/Loader";
 
 import { loadData, nodeURL } from "@/Utility/apiFuntion";
+import { GlobeLock, MapPin } from "lucide-react";
 
 const SingleDevice = () => {
   const { token } = useAuthContext();
@@ -62,7 +63,7 @@ const SingleDevice = () => {
       {/* <BackButton /> */}
       <Card className="max-h-[80vh] overflow-auto ">
         <CardHeader>
-          <CardTitle>Client Info</CardTitle>
+          <CardTitle>Client Information</CardTitle>
         </CardHeader>
         <CardContent>
           {loading ? (
@@ -70,44 +71,82 @@ const SingleDevice = () => {
               <Loader size="40" color="black" />
             </div>
           ) : (
-            <div className="flex gap-8 justify-between">
-              <div className="max-w-[50%]">
-                <p>IP Address: {info?.ip_address}</p>
-                <p>IP Organization: {info?.ip_org}</p>
-                <p>Region: {info?.region_name}</p>
-                <p>City: {info?.city}</p>
-                <p>Country: {info?.country}</p>
-                <p>Country Code: {info?.country_code}</p>
-                <p>ZIP: {info?.zip}</p>
-                <p>Time Zone: {info?.timezone}</p>
-                <p>IP License: {info?.autonomus_system}</p>
-                <p>User Agent: {info?.user_agent}</p>
-                <p>Browser Name: {info?.browser_name}</p>
-                <p>Browser Type: {info?.browser_type}</p>
-                <p>Browser Version: {info?.browser_version}</p>
-                <p>CPU Architecture: {info?.cpu_architecture}</p>
-                <p>Device Model: {info?.device_model}</p>
-                <p>Device Type: {info?.device_type}</p>
-                <p>Device Vendor: {info?.device_vendor}</p>
-                <p>Engine Name: {info?.engine_name}</p>
-                <p>Engine Version: {info?.engine_version}</p>
-                <p>Operating System Name: {info?.os_name}</p>
-                <p>Operating System Version: {info?.os_version}</p>
-              </div>
-              {locationInfos && (
-                <div className="flex flex-col gap-4">
-                  <p>Last Login Location</p>
-                  <iframe
-                    src={`https://www.google.com/maps?q=${locationInfos[0]?.latitude},${locationInfos[0]?.longitude}&z=15&output=embed`}
-                    width="300"
-                    height="300"
-                    style={{ border: 1 }}
-                    allowFullScreen
-                  ></iframe>
-                  <LocationInfoTable locationInfos={locationInfos} />
+            <div className="flex items-center gap-2">
+              <div className="bg-client-secondary rounded-lg p-3">
+                <div className="flex items-center gap-2 mb-2">
+                  <GlobeLock size={18} color="blue" />
+                  <p className="text-[14px] font-medium text-client-primary">
+                    Network Information
+                  </p>
                 </div>
-              )}
+                <p className="text-xs font-normal text-gray-500">IP Address</p>
+                <p className="text-[14px] font-medium">{info?.ip_address}</p>
+                <p className="text-xs font-normal text-gray-500">
+                  Organization
+                </p>
+                <p className="text-[14px] font-medium">{info?.ip_org}</p>
+                <p className="text-xs font-normal text-gray-500">License</p>
+                <p className="text-[14px] font-medium">
+                  {info?.autonomus_system}
+                </p>
+              </div>
+              <div className="bg-GREEN-100 rounded-lg p-3">
+                <div className="flex items-center gap-2  mb-2">
+                  <MapPin size={18} color="green" />
+                  <p className="text-[14px] font-medium text-GREEN-400">
+                    Location Details
+                  </p>
+                </div>
+                <p className="text-xs font-normal text-gray-500">IP Address</p>
+                <p className="text-[14px] font-medium">{info?.ip_address}</p>
+                <p className="text-xs font-normal text-gray-500">
+                  Organization
+                </p>
+                <p className="text-[14px] font-medium">{info?.ip_org}</p>
+                <p className="text-xs font-normal text-gray-500">License</p>
+                <p className="text-[14px] font-medium">
+                  {info?.autonomus_system}
+                </p>
+              </div>
             </div>
+            // <div className="flex gap-8 justify-between">
+            //   <div className="max-w-[50%]">
+            //     <p>IP Address: {info?.ip_address}</p>
+            //     <p>IP Organization: {info?.ip_org}</p>
+            //     <p>Region: {info?.region_name}</p>
+            //     <p>City: {info?.city}</p>
+            //     <p>Country: {info?.country}</p>
+            //     <p>Country Code: {info?.country_code}</p>
+            //     <p>ZIP: {info?.zip}</p>
+            //     <p>Time Zone: {info?.timezone}</p>
+            //     <p>IP License: {info?.autonomus_system}</p>
+            //     <p>User Agent: {info?.user_agent}</p>
+            //     <p>Browser Name: {info?.browser_name}</p>
+            //     <p>Browser Type: {info?.browser_type}</p>
+            //     <p>Browser Version: {info?.browser_version}</p>
+            //     <p>CPU Architecture: {info?.cpu_architecture}</p>
+            //     <p>Device Model: {info?.device_model}</p>
+            //     <p>Device Type: {info?.device_type}</p>
+            //     <p>Device Vendor: {info?.device_vendor}</p>
+            //     <p>Engine Name: {info?.engine_name}</p>
+            //     <p>Engine Version: {info?.engine_version}</p>
+            //     <p>Operating System Name: {info?.os_name}</p>
+            //     <p>Operating System Version: {info?.os_version}</p>
+            //   </div>
+            //   {locationInfos && (
+            //     <div className="flex flex-col gap-4">
+            //       <p>Last Login Location</p>
+            //       <iframe
+            //         src={`https://www.google.com/maps?q=${locationInfos[0]?.latitude},${locationInfos[0]?.longitude}&z=15&output=embed`}
+            //         width="300"
+            //         height="300"
+            //         style={{ border: 1 }}
+            //         allowFullScreen
+            //       ></iframe>
+            //       <LocationInfoTable locationInfos={locationInfos} />
+            //     </div>
+            //   )}
+            // </div>
           )}
         </CardContent>
       </Card>
